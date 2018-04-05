f7fe370cff21124f45ff2335ba230240c345d3f0
alikinvv <valeriy@da-group.ru>
2018-04-05 18:38:40 +0500

    fix perspective

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,19 @@
 
 $(window).load(function(){
-    $('.banner__title').addClass('show');
-    
+    $('.banner__title').addClass('show');   
+});
+
+$(document).ready(function() {    
+    $('.card').hover(function() {
+        var $this = $(this);
+        setTimeout(function() {
+            $this.find('figure').removeClass('trans');
+        },200);
+    });
+    $('.card').mouseleave(function() {
+        var $this = $(this);
+        setTimeout(function() {
+            $this.find('figure').addClass('trans');
+        },200);
+    });
 });
